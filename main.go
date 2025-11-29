2e1862da010456d7307d48cbb095240a0a991044
impact-dryer <195639122+impact-dryer@users.noreply.github.com>
2025-11-29 18:57:17 +0100

    add queue

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
+import "github.com/impact-dryer/gotattletale/pkg"
+
 func main() {
-	dev := Device{
+	dev := pkg.Device{
 		Name:        "enp18s0",
 		Description: "Ethernet interface",
 	}
