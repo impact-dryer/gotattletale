ac65530a2fb2120075d3b74d0cd78bc07e49c639
impact-dryer <195639122+impact-dryer@users.noreply.github.com>
2025-12-06 15:10:27 +0100

    use di

--- cmd/gin/main.go
+++ cmd/gin/main.go
@@ -1,75 +1,29 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
+	"github.com/impact-dryer/gotattletale/internal/config"
+	"github.com/impact-dryer/gotattletale/internal/controller"
+	"github.com/impact-dryer/gotattletale/internal/service"
 	"github.com/impact-dryer/gotattletale/pkg"
 	_ "github.com/mattn/go-sqlite3" // SQLite driver
+	"go.uber.org/fx"
 )
 
 func main() {
-	dev := pkg.Device{
-		Name:        "enp18s0",
-		Description: "Ethernet interface",
-	}
-	repository := pkg.NewSqlLitePacketRepository("db.sqlite")
-	go dev.Start()
-	go handlePackets(repository)
-	startGinServer(repository)
+	fx.New(
+		fx.Provide(config.NewAppConfig),
+		fx.Provide(pkg.NewSqlLitePacketRepository),
+		fx.Provide(service.NewPacketService),
+		fx.Provide(controller.NewPacketController),
+		fx.Invoke(service.SniffAndStorePackets),
+		fx.Invoke(pkg.CreateNewDeviceAndStartSniffing),
+		fx.Invoke(startGinServer),
+	).Run()
 }
 
-func handlePackets(repository pkg.PacketRepository) {
-	packetCache := make([]pkg.AppPacket, 0)
-	for v, ok := <-pkg.PacketsToCaptureQueue.ItemsChan; ok; v, ok = <-pkg.PacketsToCaptureQueue.ItemsChan {
-		packetCache = append(packetCache, v)
-		if len(packetCache) > 100 {
-			err := repository.SavePackets(packetCache)
-			if err != nil {
-				log.Fatal(err)
-				panic(err)
-			}
-			packetCache = make([]pkg.AppPacket, 0)
-		}
-	}
-}
-
-func startGinServer(repository pkg.PacketRepository) {
-
-	handler := PacketHandlerImpl{
-		service: PacketServiceImpl{
-			storage: repository,
-		},
-	}
+func startGinServer(packetController controller.PacketController) {
 	router := gin.Default()
-	router.GET("/api/v1/packets", handler.GetPackets)
+	router.GET("/api/v1/packets", packetController.GetPackets)
 	router.Run(":8080")
-}
-
-type PacketHandler interface {
-	GetPackets(c *gin.Context) error
-}
-
-type PacketHandlerImpl struct {
-	service PacketService
-}
-
-func (h PacketHandlerImpl) GetPackets(c *gin.Context) {
-	packets, err := h.service.GetPackets()
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-	}
-	c.JSON(200, packets)
-}
-
-type PacketService interface {
-	GetPackets() ([]pkg.SavedPacket, error)
-}
-
-type PacketServiceImpl struct {
-	storage pkg.PacketRepository
-}
-
-func (s PacketServiceImpl) GetPackets() ([]pkg.SavedPacket, error) {
-	return s.storage.GetPackets()
 }
